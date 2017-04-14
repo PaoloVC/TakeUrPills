3b05cb5a54c33d11412457dc5ec06dc242d9bb5b
Paolo Vargas <paolovc1523@gmail.com>
2017-04-14 15:52:48 -0600

    Add Pills Activity - Cancel Button Dialog Alert Functionality

--- app/src/main/java/com/una/takeurpills/AddPillActivity.java
+++ app/src/main/java/com/una/takeurpills/AddPillActivity.java
@@ -1,7 +1,12 @@
 package com.una.takeurpills;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AddPillActivity extends AppCompatActivity {
 
@@ -9,5 +14,32 @@ public class AddPillActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_pill);
+        Button cancelar = (Button) findViewById(R.id.btAddPillsCancelar);
+        cancelar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertBuilder(v);
+            }
+        });
     }
+    public void AlertBuilder(View view){
+        AlertDialog.Builder builder1 = new AlertDialog.Builder(view.getContext());
+        builder1.setMessage("Seguro que deseas cancelar los cambios?");
+        builder1.setCancelable(true);
+        builder1.setPositiveButton("Si",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        Intent intento = new Intent(getApplicationContext(), HomeActivity.class);
+                        startActivity(intento);
+                    }
+                });
+        builder1.setNegativeButton("No",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+
+                    }
+                });
+        AlertDialog alert11 = builder1.create();
+        alert11.show();
+    };
 }
