1470db4bde1c339a58b36ea44e372cabefb2ea89
Diego <dieavm@hotmail.com>
2017-04-16 22:20:37 -0600

    Agregando los datos quemados en la clase de ListPills (No agregado en el commit anterior)

--- app/src/main/java/com/una/takeurpills/ListPillsActivity.java
+++ app/src/main/java/com/una/takeurpills/ListPillsActivity.java
@@ -1,7 +1,14 @@
 package com.una.takeurpills;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class ListPillsActivity extends AppCompatActivity {
 
@@ -9,5 +16,60 @@ public class ListPillsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_pills);
+
+        FillListView();
+        OnClickListItems();
+
+        Mensaje("Listado de Tratamientos");
+
+
+    } // Fin del Oncreate de la Actividad 01
+
+    //Aca se pueden cargar los tratamientos desde el Json  del app, pero solo los nombres (posible a cambios)
+    private void FillListView() {
+        String[] pills = {
+                "Acetaminofen",
+                "Paracetamol",
+                "Ibuprofeno",
+                "Flumocil",
+                "Acetaminofen",
+                "Paracetamol",
+                "Ibuprofeno",
+                "Flumocil",
+                "Acetaminofen",
+                "Paracetamol",
+                "Ibuprofeno",
+                "Flumocil",
+                "Acetaminofen",
+                "Paracetamol",
+                "Ibuprofeno",
+                "Flumocil"
+        };
+
+        ArrayAdapter<String> adaptador = new ArrayAdapter(this,
+                android.R.layout.simple_list_item_1, pills);
+        ListView milistview = (ListView) findViewById(R.id.listPills);
+        milistview.setAdapter(adaptador);
     }
+
+    //Se cambia este metodo para enviar a pantalla de Detalles del tratamiento
+    public void OnClickListItems() {
+        ListView list = (ListView) findViewById(R.id.listPills);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> paret, View viewClicked,
+                                    int position, long id) {
+                Intent intento = new Intent(getApplicationContext(), DetailsActivity.class);
+                startActivity(intento);
+                TextView textView = (TextView) viewClicked;
+                String message = "Tratamiento # " + (1 + position) + ", corresponde a: " + textView.getText().toString();
+                Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    public void Mensaje(String msg) {
+        getSupportActionBar().setTitle(msg);
+    };
+
 }
