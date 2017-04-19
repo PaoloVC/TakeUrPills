81a035e1de20181619a50c9b1b9e1e4efa875d14
Bryan Jesús Murillo Rodríguez <Bryan Jess Murillo Rodrguez>
2017-04-19 10:38:26 -0600

    Bryan-ListPills

--- app/src/main/java/com/una/takeurpills/ListPillsActivity.java
+++ app/src/main/java/com/una/takeurpills/ListPillsActivity.java
@@ -149,7 +149,7 @@ public class ListPillsActivity extends AppCompatActivity {
         for (int i = 0; i < len; i++) {
             final JSONObject obj = ja.optJSONObject(i);
             try {
-                String nombre = String.valueOf(obj.get("nombre"));
+                String nombre = String.valueOf(obj.get("titulo"));
             }
             catch(JSONException exc){
             }
